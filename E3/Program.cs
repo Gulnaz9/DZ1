9da60cba2319744c0b703b5ff3246b06dabaf5ba
Гульназ Калимуллина <gulnaz.kalimullina.2005@mail.ru>
2023-09-16 21:54:14 +0300

    пополнение

---
+++ E3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности"); //радиус только целое число
+            int R = int.Parse(Console.ReadLine());
+            double l = 2 * Math.PI * R; //Math.PI константа
+            Console.WriteLine("Длина окружности равна " + l);
+            double S = Math.PI * R * R;
+            Console.WriteLine("Площадь круга равна " + S);
+            Console.ReadKey();
+
+            /* второй вариант
+             * int stepen = 2;
+             * double S = Math.PI * Math.Pow(R, stepen); 
+             * Console.WriteLine("Площадь круга равна " + S);
+             */
+        }
+    }
+}
---
+++ E4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите градусную меру угла");
+            double degree = int.Parse(Console.ReadLine());
+            double x = (degree * Math.PI) / 180; //переводит градусы в радианы
+            double Y = Math.Cos(x);
+
+            Console.WriteLine("Y=cos(" + degree + ")=" + Y);
+            Console.ReadKey();
+
+        }
+    }
+}
